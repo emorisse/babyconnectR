5b48b025209cb468937947b862ab6f214577e99b
Erich Morisse <erich.morisse@gmail.com>
2014-09-03 14:26:39 -0400

    inital code drop

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 babyconnectR
 ============
+
+* drop your csv files in data/
+* run the baby.R script
+
+# outputs
+* hours.png - clusters show comparing start.ct (date and time) to start.hour (hour of the day)
+* durationHour.png - comparison of start.hour and how long each feeding lasted
+* durationStart.png - comparison of start.ct and how long each feeding lasted
+
+For my son, only durationStart.png showed any patterns discernable visually.
---
+++ baby.R
@@ -0,0 +1,37 @@
+options(stringsAsFactors = FALSE)
+library(mclust)
+library(ggplot2)
+
+filenames <- list.files("data", pattern="*.csv", full.names=TRUE)
+tables <- lapply(filenames, read.csv)
+data <- do.call(rbind, tables)
+data <- unique(data)
+
+c2 <- data[data$Activity=="Nursing",c(1,2,4)]
+c <- data.frame(
+	as.POSIXct(c2[,1],format="%m/%d/%y %H:%M"),
+	as.POSIXct(c2[,2],format="%m/%d/%y %H:%M"),
+	c2[,3],
+	rep(0,nrow(c2)),
+	rep(0,nrow(c2))
+)
+names(c) <- c("start.ct", "end.ct", "duration", "start.hour", "end.hour")
+c$start.hour <- as.POSIXlt(c$start.ct)$hour + as.POSIXlt(c$start.ct)$min/60 + as.POSIXlt(c$start.ct)$sec/3600
+c$end.hour   <- as.POSIXlt(c$end.ct)$hour + as.POSIXlt(c$end.ct)$min/60 + as.POSIXlt(c$end.ct)$sec/3600
+
+fit <- Mclust(c)
+hours<-qplot(start.ct, jitter(start.hour), col=factor(fit$classification),data=c) + scale_colour_brewer(palette="Set1","cluster")
+png("hours.png")
+hours
+dev.off()
+
+durationHour <-qplot(jitter(start.hour), duration, col=factor(fit$classification),data=c) + scale_colour_brewer(palette="Set1","cluster")
+png("durationHour.png")
+durationHour
+dev.off()
+
+durationStart <- qplot(start.ct, duration, col=factor(fit$classification),data=c) + scale_colour_brewer(palette="Set1","cluster")
+png("durationStart.png")
+durationStart
+dev.off()
+
